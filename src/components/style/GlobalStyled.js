574fe12cf567e1d7931947924550a24aa66838fa
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-02-27 22:13:15 -0300

    responsive

--- src/components/style/GlobalStyled.js
+++ src/components/style/GlobalStyled.js
@@ -17,28 +17,43 @@ export const Container = styled.div `
 `
 export const DisplayComponents = styled.div `
     width:100vw; 
+    background-color:white;
     display:flex; 
     justify-content:space-around;   
     flex-direction:column;  
-    height: 99vh;
+    height: 100vh;
     margin:auto;
+    @media (min-width:730px ) and (max-width: 900px){
+    height: 100vh;
+    }
 `
 export const SeachContainer = styled.div `
-    width:100vw; 
-    margin: auto;
-    padding:20px; 
+    width:100%; 
+    margin: auto; 
     height: 50vh;
     display:flex;
     flex-direction:column;
     justify-content:center;
     background-color:white;
+    @media (min-width:730px) and (max-width: 900px){
+    width:100%; 
+    }
 `
 export const ReturnContainer = styled.div `
     width:100%; 
     background-color:whitesmoke;
     margin: auto;
-    padding:20px; 
+    display:flex;
+    justify-content:center;
+    flex-direction: column;
     height: 50vh;
+    @media (min-width:730px ) and (max-width: 900px){
+    width:100%; 
+    }
+    @media (min-width:901px ) and (max-width: 1700px) {
+    width:100%; 
+    height: 50vh;
+    } 
    
 `
 export const SeachInput = styled.input `
@@ -51,6 +66,13 @@ export const SeachInput = styled.input `
     border:none;
     background-color:transparent;
     outline: 0;
+    @media (min-width:730px ) and (max-width: 900px){
+    width:400px;
+    }
+    @media (min-width:299px) and (max-width: 729px) {
+    width: 280px;
+    }
+   
 
 `
 export const ContainerSeach = styled.div `
@@ -60,6 +82,12 @@ export const ContainerSeach = styled.div `
     border:solid 2px #cdcdcd;
     display:flex;
     justify-content:space-around;   
+    @media (min-width:730px) and (max-width: 900px){
+    width:450px;
+    }
+    @media (min-width:299px) and (max-width: 729px) {
+    width: 300px;
+    }
 
 `
 export const ButtonSeach  = styled.button `
@@ -72,24 +100,68 @@ export const ButtonSeach  = styled.button `
     justify-content:center; 
     padding:10px;
     color:#cdcdcd;
+    @media (min-width:730px ) and (max-width: 900px){
+    font-size:1.5em;
+    }
+    @media (min-width:299px) and (max-width: 729px) {
+    font-size: 1.2em;
+    }
+    @media (min-width:299px) and (max-width: 729px) {
+    font-size: 1.2em;
+    }
 `
 export const ReturnInfo = styled.p `
     color:gray;
     font-size:1.2em;
     text-align:left;
     width:600px;
-    padding:20px;
+    padding:10px;
+    @media (min-width:730px) and (max-width: 900px) {
+    padding: 10px;
+    font-size: 1em;
+    }  
+    @media (min-width:901px ) and (max-width: 1700px) {
+    font-size: 1em;
+    padding:10px;   
+    } 
+    @media (min-width:299px) and (max-width: 729px) {
+    font-size: 1em;
+    padding:5px; 
+    width:200px;
+    }
 `
 export const Information = styled.h1 ` 
     color:gray;
     font-size:2em;
     text-align:left;
-    padding:20px;   
+    padding:10px; 
+    @media (min-width:730px ) and (max-width: 900px) {
+    padding: 10px;
+    font-size: 1.5em;
+    }  
+    @media (min-width:901px ) and (max-width: 1700px) {
+    font-size: 1.5em;
+    } 
+    @media (min-width:299px) and (max-width: 729px) {
+    font-size: 1.2em;
+    }
 
 `
 export const ReturnContainerText = styled.div `
-    width:900px; 
-    margin: auto;
-    padding:20px; 
+    width:700px; 
+    margin: auto; 
     margin:auto;
+    background:whitesmoke;
+    @media (min-width:730px) and (max-width: 900px) {
+    width:70%;
+    margin:auto;
+    }
+    @media (min-width:901px ) and (max-width: 1700px) {
+    width:50%;
+    margin:auto;
+    }
+    @media (min-width:299px) and (max-width: 729px) {
+    width:300px;
+    margin:auto;
+    }
 `
